a4e1c0b7f7f6bb763a705614c0d0dc116b47e7b0
Caleb Palmer <palmer.caleb@gmail.com>
2016-11-06 17:07:06 -0400

    Some better graphics

--- player.cpp
+++ player.cpp
@@ -1,8 +1,9 @@
 #include "player.h"
 
 #include "dumb_graphics_component.h"
-#include "player_input_component.h"
 #include "player_physics_component.h"
+#include "player_input_component.h"
+#include "player_graphics_component.h"
 
 using namespace std;
 using namespace CapEngine;
@@ -12,7 +13,7 @@ shared_ptr<GameObject> makePlayer(Uint32 windowID, ControlScheme* pControlScheme
 
   shared_ptr<InputComponent> pInputComponent(new PlayerInputComponent(pControlScheme));
   shared_ptr<PhysicsComponent> pPhysicsComponent(new PlayerPhysicsComponent());
-  shared_ptr<GraphicsComponent> pGraphicsComponent(new DumbGraphicsComponent(windowID, 40, 60, Colour(0, 255, 0)));
+  shared_ptr<GraphicsComponent> pGraphicsComponent(new PlayerGraphicsComponent(windowID, 40, 60));
   shared_ptr<CustomComponent> pCustomComponent(new NullCustomComponent());
   shared_ptr<AIComponent> pAIComponent(new NullAIComponent());
 
