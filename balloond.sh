abd969f8493ce320bace9dd1b7b52771047e166a
Caleb Palmer <palmer.caleb@gmail.com>
2016-11-29 21:11:12 -0400

    - gdb opens in tui mode

--- balloond.sh
+++ balloond.sh
@@ -4,5 +4,5 @@ curdir=$(pwd)
 export PATH=$curdir/capengine:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$curdir/capengine:.
 
-gdb -ex "dir capengine" balloon
+gdb -tui -ex "dir capengine" balloon
 
