e19bf360480cf275cd58272a60e0fe1c6c5acc18
Caleb Palmer <palmer.caleb@gmail.com>
2016-12-11 12:49:22 -0400

    Adds documentation

--- control_scheme.h
+++ control_scheme.h
@@ -15,9 +15,29 @@
 class ControlScheme {
  public:  
   virtual ~ControlScheme() {}
+  /**
+     Add a ControlSchemeListener that will be notified of any actions in
+     the control schems
+     @param pListener The ControlSchemeListener
+   */
   virtual void listen(ControlSchemeListener* pListener);
+
+  /**
+     Remove a ControlSchemeListener so that it is no longer listening for 
+     actions on this control schems
+   */
   virtual void unlisten(ControlSchemeListener* pListener) ;
+
+  /**
+     Disables the control scheme so that it ignores any Input devices 
+     it is listening to.  Useful if a GameState still exists but is not in the foreground.
+     It is disabled by default.
+   */
   virtual void disable();
+
+  /**
+     Enables the control scheme so that it starts listening to events.
+   */
   virtual void enable();
 
  protected:
