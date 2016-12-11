be084b72cfca7c3da2cf03856ed31e6895b86125
Caleb Palmer <palmer.caleb@gmail.com>
2016-12-11 12:54:48 -0400

    Adds documentation for ControlSchemeListener

--- control_scheme_listener.cpp
+++ control_scheme_listener.cpp
@@ -1,16 +1,10 @@
 #include "control_scheme_listener.h"
 
-/**
-   This registers the listener with a specific control scheme
- */
 void ControlSchemeListener::subscribe(ControlScheme* pControlScheme){
   pControlScheme->listen(this);
   m_pControlSchemes.push_back(pControlScheme);
 }
 
-/**
-   Destructor:  unregisters from the ControlScheme
- */
 ControlSchemeListener::~ControlSchemeListener(){
   for(size_t i = 0; i < m_pControlSchemes.size(); i++){
     (m_pControlSchemes[i])->unlisten(this);
--- control_scheme_listener.h
+++ control_scheme_listener.h
@@ -6,14 +6,28 @@
 
 #include "control_scheme.h"
 
+/**
+   This class defines an object that listens to a ControlScheme(s) for actions
+   associated with that ControlScheme.
+ */
 class ControlSchemeListener {
  public:
+  /**
+     Receives a message from a ControlScheme
+     @param input The message received.
+   */
   virtual void receiveInput(std::string input) = 0;
+
+  /**
+     Registers itself to listen for messages from the give ControlScheme
+     #param pControlScheme the ControlScheme to listen to.
+   */
   virtual void subscribe(ControlScheme* pControlScheme);
+  
   virtual ~ControlSchemeListener();
 
  private:
-  std::vector<ControlScheme*> m_pControlSchemes;
-};
+  std::vector<ControlScheme*> m_pControlSchemes;  // keeps track of ControlSchemes listening to.
+}; 
 
 #endif // CONTROL_SCHEME_LISTENER
